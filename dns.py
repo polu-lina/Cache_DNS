30b9f81738d2c827e712a1bc39130296e113c54d
Alina Polupanova <34677605+polu-lina@users.noreply.github.com>
2020-06-08 00:03:20 +0500

    Update dns.py

--- dns.py
+++ dns.py
@@ -11,41 +11,40 @@ class DNS():
         self.response.settimeout(1)
 
 
-    def process(self):
-        while True:
-            try:
-                data, address = self.request.recvfrom(512)
-                if data:
-                    parse_data = dnslib.DNSRecord.parse(data)
-                    question = parse_data.questions[0]
-                    if question.qname in self.cache and question.qtype in cache[question.qname]:
-                        info, ttl = self.cache[question.qname]
-                        print('Info from cache:\t' + info)
-                        parse_data.questions.remove(question)
-                    else:
-                        server = ("8.8.4.4", 53)
-                        self.response.sendto(parse_data.pack(), server)
+    def run(self):
+        try:
+            data, address = self.request.recvfrom(512)
+            if data:
+                parse_data = dnslib.DNSRecord.parse(data)
+                question = parse_data.questions[0]
+                if question.qname in self.cache and question.qtype in self.cache[question.qname]:
+                    info, _ = self.cache[question.qname][question.qtype]
+                    print('Info from cache:\n' + str(info))
+                    parse_data.questions.remove(question)
+                else:
+                    server = ("8.8.8.8", 53)
+                    self.response.sendto(parse_data.pack(), server)
 
-                time.sleep(0.25)
-
-                data, address = self.response.recvfrom(512)
-                current_time = int(time.time())
-                if data:
-                    parse_data = dnslib.DNSRecord.parse(data)
-                    print(parse_data)
-                    for question in parse_data.rr:
-                        self.cache[question.rname]= (parse_data, current_time + question.ttl)
-            except OSError:
-                pass
             time.sleep(0.25)
-            self.update_cache()
+
+            data, address = self.response.recvfrom(512)
+            if data:
+                parse_data = dnslib.DNSRecord.parse(data)
+                print(parse_data)
+                for question in parse_data.rr:
+                    self.cache[question.rname]= {question.rtype: (parse_data, int(time.time()) + question.ttl)}
+        except OSError:
+            pass
+        time.sleep(0.25)
+        self.update_cache()
 
     def update_cache(self):
-        current_time = int(time.time())
         for i in self.cache:
-            _, ttl = self.cache[i]
-            if ttl < current_time:
-                del self.cache[i]
+            current_time = int(time.time())
+            for j in self.cache[i]:
+                _, ttl = self.cache[i][j]
+                if ttl < current_time:
+                    del self.cache[i][j]
                   
 
 
@@ -58,16 +57,16 @@ if __name__ == '__main__':
                 server.update_cache()
     except Exception:
         pass
-    print("Server is running")
-    command = input("Введите 'stop', если хотите остановить сервер, введите 'check', если хотите протестировать его")
+    print("Сервер запущен")
     try:
         while True:
-            if command == "client":
-                while True:
-                    address = input("Введите адрес")
-                    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                    s.sendto(dnslib.DNSRecord.question(str(address)).pack(), ("localhost", 53))
-                    s.close()
+            command = input("\nВведите 'stop', если хотите остановить сервер, введите 'check', если хотите протестировать его\n")
+            if command == "check":
+                address = input("Введите адрес\n")
+                s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                s.sendto(dnslib.DNSRecord.question(str(address)).pack(), ("localhost", 53))
+                s.close()
+                server.run()
             elif command == "stop":
                 break
             else:
